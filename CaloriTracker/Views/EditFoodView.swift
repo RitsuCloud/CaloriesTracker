d5282a784f168576f7e9218b69f2027ff3c6cf84
RitsuCloud <chonhinchou@gmail.com>
2024-09-12 23:07:27 -0700

    Added editing and deleting function for each food entry

---
+++ CaloriTracker/Views/EditFoodView.swift
@@ -0,0 +1,18 @@
+//
+//  EditFoodView.swift
+//  CaloriTracker
+//
+//  Created by Chon Hin Chou on 9/12/24.
+//
+
+import SwiftUI
+
+struct EditFoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditFoodView()
+}
